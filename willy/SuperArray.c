4dc200052208091858f21706b5a1d1e77ccfdf09
Wiilver <susareywvh@gmail.com>
2026-08-14 21:25:00 -0600

    Habra genericos hasta que los borre por completo

--- willy/SuperArray.c
+++ willy/SuperArray.c
@@ -1,69 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
-//Para errores
+/*
+    Hay un problema con la naturaleza en si de la distincion entre arreglos,
+    deberia de replantear el codigo para que, en vez de tener una plantilla
+    para cada tipo, tener las funciones indicadas para hacer cada cosa, creo
+*/
 
-int checarNulo(void* ptr){
-    return (ptr==NULL);
-}
-
-int checarIndice(int indice, int longitud){
-    return ((indice >= longitud)||indice < 0);
-}
-
-//Estan mal los punteros
+//Genericos
 struct Arreglo{
+    void* arreglo;
     int longitud;
-    void *arreglo;
-    int (*iniciar)(void*, int);
-    int (*imprimir)(void*, int, int);
+    int tama;
+    char tipo;
 };
 
-//Enteros
-int intIni(void* arr, int longitud){
-    arr = calloc(longitud, sizeof(int));
-    return checarNulo(arr);
+void iniciar(struct Arreglo* arr, int longitud, char tipo){
+    (*arr).longitud = longitud;
+    (*arr).tipo = tipo;
+    switch(tipo){
+        case 'I':
+            (*arr).tama = sizeof(int);
+            break;
+        case 'D':
+            (*arr).tama = sizeof(double);
+            break;
+    }
+    (*arr).arreglo = calloc(longitud, (*arr).tama);
+    assert((*arr).arreglo != NULL);
 }
 
-int intImp(void* arr, int indice, int longitud){
-    if(checarIndice(indice, longitud)) return 1;
-    int *p = arr;
-    printf("El valor %d de tu arreglo es igual a : %d\n", indice, p[indice]);
-    return 0;
+
+struct ArrInt{
+    struct Arreglo propiedades;
+    void (*iniciar)(struct ArrInt*, int);
+    void (*cambiar)(struct ArrInt*, int, int);
+    void (*imprimir)(struct ArrInt*);
+};
+
+void intImp(struct ArrInt *arr){
+    int* a = (*arr).propiedades.arreglo;
+    int i;
+    for(i = 0; i < (*arr).propiedades.longitud; i++) printf("El valor numero %d de tu arreglo es igual a : %d\n", i+1, a[i]);
 }
 
-//Dobles
-int dobIni(void* arr, int longitud){
-    arr = calloc(longitud, sizeof(double));
-    return checarNulo(arr);
+void intCam(struct ArrInt* arr, int indice, int valor){
+    assert(((*arr).propiedades.longitud > indice)&&(indice > 0));
+    int* a = (*arr).propiedades.arreglo;
+    a[indice] = valor;
 }
 
-int dobImp(void* arr, int indice, int longitud){
-    if(checarIndice(indice, longitud)) return 1;
-    double *p = arr;
-    printf("El valor %d de tu arreglo es igual a : %lf\n", indice, p[indice]);
-    return 0;
+void intIni(struct ArrInt* arr, int longitud){
+    iniciar(&(*arr).propiedades, longitud, 'I');
+    (*arr).imprimir = intImp;
+    (*arr).cambiar = intCam;
 }
+
+
+struct ArrDob{
+    struct Arreglo propiedades;
+    void (*iniciar)(struct ArrDob*, int);
+    void (*cambiar)(struct ArrDob*, int, double);
+    void (*imprimir)(struct ArrDob*);
+};
+
+void dobImp(struct ArrDob*arr){
+    double* a = (*arr).propiedades.arreglo;
+    int i;
+    for(i = 0; i < (*arr).propiedades.longitud; i++) printf("El valor numero %d de tu arreglo es igual a : %lf\n", i+1, a[i]);
+}
+
+void dobCam(struct ArrDob* arr, int indice, double valor){
+    assert(((*arr).propiedades.longitud > indice)&&(indice > 0));
+    double* a = (*arr).propiedades.arreglo;
+    a[indice] = valor;
+}
+
+void dobIni(struct ArrDob* arr, int longitud){
+    iniciar(&(*arr).propiedades, longitud, 'D');
+    (*arr).imprimir = dobImp;
+    (*arr).cambiar = dobCam;
+}
+
 
 int main(){
-    struct Arreglo enteros;    
-    struct Arreglo dobles;    
-
-    enteros.longitud = 5;
-    dobles.longitud = 5;
-
+    struct ArrInt enteros;
     enteros.iniciar = intIni;
-    enteros.imprimir = intImp;
-    
+    enteros.iniciar(&enteros, 5);
+
+    struct ArrDob dobles;
     dobles.iniciar = dobIni;
-    dobles.imprimir = dobImp;
+    dobles.iniciar(&dobles, 5);
 
-    enteros.iniciar(&enteros.arreglo, enteros.longitud);
-    dobles.iniciar(&dobles.arreglo, dobles.longitud);
+    dobles.cambiar(&dobles, 0, 2.5);    
+    dobles.cambiar(&dobles, 1, 4.5);    
+    dobles.cambiar(&dobles, 2, 6.5);    
+    dobles.cambiar(&dobles, 3, 8.5);    
+    dobles.cambiar(&dobles, 4, 10.5);    
 
-    enteros.imprimir(&enteros.arreglo, 0, enteros.longitud);
-    dobles.imprimir(&dobles.arreglo, 0, dobles.longitud);
+    enteros.cambiar(&enteros, 0, 2);
+    enteros.cambiar(&enteros, 1, 4);
+    enteros.cambiar(&enteros, 2, 6);
+    enteros.cambiar(&enteros, 3, 8);
+    enteros.cambiar(&enteros, 4, 10);
 
+    enteros.imprimir(&enteros);
+    printf("\n");
+    dobles.imprimir(&dobles);
 
     return 0;
 }
